590b815e45f37946f79fe4bdfd1fd05ec74429bf
Rick Moynihan <rick.m@swirrl.com>
2015-10-12 11:06:48 +0100

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-build-env "1.0.0-SNAPSHOT"
+(defproject lein-build-env "1.0.0"
   :description "Middleware to Rewrite part of a leiningen build coordinate with
   a build number supplied by an environment variable."
   :url "https://github.com/rickmoynihan/lein-build-env"
